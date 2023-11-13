50159dfc9d354ac5c02bea7ef7633293374922d9
AdhiPuss <adhipuspo@gmail.com>
2023-11-13 21:04:09 +0700

    video 8

--- router/users.js
+++ router/users.js
@@ -8,11 +8,31 @@ let users = [
 ]
 
 router.get('/users', (req, res) => {
+    if(users.length > 0){
+        res.json({
+            status: true,
+            data: users,
+            method: req.method,
+            url: req.url
+        })
+    }else{
+        res.json({
+            status: false,
+            message: "Data masih kosong"
+        })
+    }
     res.json(users)
   })
 
 router.post('/user', (req, res) => {
     users.push(req.body)
+    res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+        message: "Data berhasil ditambahkan"
+    })
     res.json(users)
 })
 
@@ -25,14 +45,26 @@ router.put('/user/:id', (req, res) => {
             return user
         }
     })
-    res.json(users)
+    res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+        message: "Data berhasil diubah"
+    })
 })
 
 router.delete('/user/:id', (req, res) => {
     const id = req.params.id
     users = users.filter(user => user.id != id)
 
-    res.json(users)
+    res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+        message: "Data berhasil dihapus"
+    })
   })
 
   module.exports = router
