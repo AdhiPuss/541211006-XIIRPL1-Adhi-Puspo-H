f1eb514f803e5ccfb2caac764837d4a2096bb44b
AdhiPuss <adhipuspo@gmail.com>
2023-11-13 19:45:58 +0700

    video 5

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 const userrouter = require('./router/users')
+
 const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
